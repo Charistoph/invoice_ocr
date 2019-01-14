0f43c598bd7a6daf13f3bdc3fb9c4ce5853f4584
Charistoph <christoph.bernkopf95@gmail.com>
2019-01-14 23:57:33 +0100

    first ideas

---
+++ gen_invoices.py
@@ -0,0 +1,72 @@
+df["company_name"] = "easyname GmbH"
+df["company_address"] = "Fernkorngasse 10/3/501 1100 Vienna, Austria"
+df["company_email"] = "billing@easyname.com"
+df["company_website"] = "www.easyname.com"
+df["customer_address"] = "Christoph Bernkopf Bellariastrasse 12/3 1010 Wien (AT)"
+df["header"] = "Invoice"
+df["customer_number_title"] = "Customer no."
+df["customer_number"] = "Customer no.: 47253"
+df["invoice_number_title"] = "Invoice:"
+df["invoice_number"] = "RE0669389"
+df["filler1"] = "Pos. Count Description"
+df["product_1"] = "1 1 Domain: skillfinity.io Renewal 1 year"
+df["filler_2"] = "Your new balance is: EUR 0.00"
+df["bill_date_title"] = "Form date"
+df["bill_date"] = "25.12.2018"
+df["delivery_date_title"] = "Delivery date"
+df["delivery_date"] = "25.12.2018"
+df["filler_3"] = "You do not need to pay this invoice - it has already been paid."
+df["subtotal_title"] = "Price (incl. VAT) VAT"
+df["subtotal_currency"] = "EUR"
+df["subtotal"] = "57.50"
+df["price_title"] = "Price (incl. VAT) VAT"
+df["price_currency"] = "EUR"
+df["price"] = "69.00"
+
+arr = []
+arr.append("easyname GmbH")
+arr.append("Fernkorngasse 10/3/501 1100 Vienna, Austria")
+arr.append("billing@easyname.com")
+arr.append("www.easyname.com")
+arr.append("Christoph Bernkopf Bellariastrasse 12/3 1010 Wien (AT)")
+arr.append("Invoice")
+arr.append("Customer no.")
+arr.append("Customer no.: 47253")
+arr.append("Invoice:")
+arr.append("RE0669389")
+arr.append("Pos. Count Description")
+arr.append("1 1 Domain: skillfinity.io Renewal 1 year")
+arr.append("Your new balance is: EUR 0.00")
+arr.append("Form date")
+arr.append("25.12.2018")
+arr.append("Delivery date")
+arr.append("25.12.2018")
+arr.append("You do not need to pay this invoice - it has already been paid.")
+arr.append("Price (incl. VAT) VAT")
+arr.append("EUR")
+arr.append("57.50")
+arr.append("Price (incl. VAT) VAT")
+arr.append("EUR")
+arr.append("69.00")
+
+fillers_lines
+---------------
+-----------------------------
+_______________
+_____________________________
+
+
+fillers:
+You do not need to pay this invoice - it has already been paid.
+
+
+endings
+Thank you for shopping.
+When paying by SEPA direct debit mandate: The amount due will be debited in 3 days from your bank account.
+The general terms and conditions of easyname apply, which can be found at https: // www.easyname.com/agb
+
+
+
+
+
+
